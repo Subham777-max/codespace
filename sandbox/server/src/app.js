187c284acb75621b89c9e9543d969f68452ce11e
Subham777-max <subhamray865@gmail.com>
2026-07-19 16:31:53 +0530

    feat: add endpoint to start sandbox with pod and service creation

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import { createPod } from "./kubernetes/pod.js";
+import { createService } from "./kubernetes/service.js";
+import { v7 as uuid} from "uuid";
 
 const app = express();
 app.use(morgan("dev"));
@@ -8,6 +11,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/sandbox/health", (req, res) => {
   res.status(200).json({ status: "ok" });
+});
+
+app.post("/api/sandbox/start", async (req, res) => {
+  const sandboxId = uuid();
+  try {
+    await Promise.all([
+      createPod(sandboxId),
+      createService(sandboxId)
+    ]);
+    res.status(201).json({ 
+      message: "Sandbox started successfully",
+      sandboxId: sandboxId,
+      previewUrl: `http://${sandboxId}.preview.localhost`
+     });
+  } catch (error) {
+    console.error("Error creating pod or service:", error);
+    res.status(500).json({ error: "Failed to start sandbox" });
+  }
 });
 
 export default app;
